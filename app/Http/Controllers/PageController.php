3d73d98c0ce280398ca8f40cdbc9470e9fa09ea9
John C. Lendvoy <johntheleatherman@gmail.com>
2019-11-18 12:17:24 -0700

    Swapped images on home page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,23 +20,26 @@ class PageController extends Controller
 			[
 				'title' => 'Software',
 				'link' => '/software',
-				'image' => '/images/home/procedural_mountains.png',
-				'image_alt' => 'Screenshot of Procedural Mountains',
+				// 'image' => '/images/home/procedural_mountains.png',
+				'image' => '/svg/undraw_programming.svg',
+				'image_alt' => 'Programmer with multiple screens',
 				'description' => 'I\'ve used many different technologies to develop games, software tools and other experiments.',
 				'button_text' => 'View Software'
 			],
 			[
 				'title' => 'Web Design',
 				'link' => '/design',
-				'image' => '/images/home/tech_company.png',
-				'image_alt' => 'Tech company homepage designed in tailwind',
+				// 'image' => '/images/home/tech_company.png',
+				'image' => '/svg/undraw_experience_design.svg',
+				'image_alt' => 'Web Design',
 				'description' => 'I practice my design skills by making interfaces for websites and web apps with CSS, HTML5, and <a href="http://tailwindcss.com" target="_blank">Tailwind</a>.',
 				'button_text' => 'View Designs'
 			],
 			[
 				'title' => 'Digital Art',
 				'link' => '/art',
-				'image' => '/images/home/codezillla_small.jpg',
+				// 'image' => '/images/home/codezillla_small.jpg',
+				'image' => '/svg/undraw_art.svg',
 				'image_alt' => 'Codezillla logo',
 				'description' => 'I produce music on my computer as well as program visuals for music videos using <a href="http://processing.org" target="_blank">Processing.</a>',
 				'button_text' => 'Watch/Listen'
